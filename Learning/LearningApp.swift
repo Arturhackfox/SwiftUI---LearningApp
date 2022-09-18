e43bc51e6bc4fd99e71abebedd529c80c37b4e49
arthur <ERHUE@EX.UA>
2022-09-18 13:58:20 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Arthur Sh on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
